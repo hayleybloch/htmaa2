54dc8b73588b41626357620a9a005ecfa2c2de2d
Hayley Bloch <hayleybloch@users.noreply.github.com>
2025-09-28 15:49:25 -0400

    Fix remaining desktop app asset paths
    - Updated SkillsView component icon paths to use /htmaa2/desktop/ prefix
    - Fixed CSS font paths in globals.css for century_schoolbook.ttf and noto_serif_toto.ttf
    - Fixed spritesheet icon path in CSS
    - All desktop app assets now use correct subdirectory paths
    - Should resolve all remaining 404 errors for icons and fonts

--- apps/desktop/applications/Skills/SkillsView.tsx
+++ apps/desktop/applications/Skills/SkillsView.tsx
@@ -34,36 +34,36 @@ export default function SkillsView(props: WindowProps) {
 
           <h1>Programming</h1>
 <ul>
-  <li><SkillEntry skill="Python" iconSrc="/icons/skills/python.svg?v=2" iconAlt="Python" /></li>
-  <li><SkillEntry skill="Arduino (C++)" iconSrc="/icons/skills/arduino.svg" iconAlt="Arduino" /></li>
-  <li><SkillEntry skill="JavaScript / HTML / CSS" iconSrc="/icons/skills/javascript.svg" iconAlt="JavaScript" /></li>
+  <li><SkillEntry skill="Python" iconSrc="/htmaa2/desktop/icons/skills/python.svg?v=2" iconAlt="Python" /></li>
+  <li><SkillEntry skill="Arduino (C++)" iconSrc="/htmaa2/desktop/icons/skills/arduino.svg" iconAlt="Arduino" /></li>
+  <li><SkillEntry skill="JavaScript / HTML / CSS" iconSrc="/htmaa2/desktop/icons/skills/javascript.svg" iconAlt="JavaScript" /></li>
 </ul>
 
 <h1>Electronics & Prototyping</h1>
 <ul>
-  <li><SkillEntry skill="PCB Design (KiCad)" iconSrc="/icons/skills/kicad.svg" iconAlt="KiCad" /></li>
-  <li><SkillEntry skill="Microcontrollers (Arduino, nRF52, Raspberry Pi)" iconSrc="/icons/skills/cpu.svg" iconAlt="Microcontrollers" /></li>
-  <li><SkillEntry skill="Soldering & Circuit Prototyping" iconSrc="/icons/skills/soldering.svg?v=2" iconAlt="Soldering" /></li>
+  <li><SkillEntry skill="PCB Design (KiCad)" iconSrc="/htmaa2/desktop/icons/skills/kicad.svg" iconAlt="KiCad" /></li>
+  <li><SkillEntry skill="Microcontrollers (Arduino, nRF52, Raspberry Pi)" iconSrc="/htmaa2/desktop/icons/skills/cpu.svg" iconAlt="Microcontrollers" /></li>
+  <li><SkillEntry skill="Soldering & Circuit Prototyping" iconSrc="/htmaa2/desktop/icons/skills/soldering.svg?v=2" iconAlt="Soldering" /></li>
 </ul>
 
 <h1>Digital Fabrication</h1>
 <ul>
-  <li><SkillEntry skill="3D Modeling (Fusion 360, Blender)" iconSrc="/icons/skills/blender.svg" iconAlt="Blender" /></li>
-  <li><SkillEntry skill="3D Printing (SLA & FDM)" iconSrc="/icons/skills/printer.svg" iconAlt="3D Printing" /></li>
-  <li><SkillEntry skill="Laser Cutting" iconSrc="/icons/skills/laser.svg" iconAlt="Laser Cutting" /></li>
+  <li><SkillEntry skill="3D Modeling (Fusion 360, Blender)" iconSrc="/htmaa2/desktop/icons/skills/blender.svg" iconAlt="Blender" /></li>
+  <li><SkillEntry skill="3D Printing (SLA & FDM)" iconSrc="/htmaa2/desktop/icons/skills/printer.svg" iconAlt="3D Printing" /></li>
+  <li><SkillEntry skill="Laser Cutting" iconSrc="/htmaa2/desktop/icons/skills/laser.svg" iconAlt="Laser Cutting" /></li>
 </ul>
 
 <h1>Creative</h1>
 <ul>
-  <li><SkillEntry skill="Ceramics & Sculpture" iconSrc="/icons/skills/ceramics.svg" iconAlt="Ceramics" /></li>
-  <li><SkillEntry skill="Fashion & Performance" iconSrc="/icons/skills/fashion.svg" iconAlt="Fashion" /></li>
-  <li><SkillEntry skill="Visual Design (Figma, Canva)" iconSrc="/icons/skills/figma.svg" iconAlt="Figma" /></li>
+  <li><SkillEntry skill="Ceramics & Sculpture" iconSrc="/htmaa2/desktop/icons/skills/ceramics.svg" iconAlt="Ceramics" /></li>
+  <li><SkillEntry skill="Fashion & Performance" iconSrc="/htmaa2/desktop/icons/skills/fashion.svg" iconAlt="Fashion" /></li>
+  <li><SkillEntry skill="Visual Design (Figma, Canva)" iconSrc="/htmaa2/desktop/icons/skills/figma.svg" iconAlt="Figma" /></li>
 </ul>
 
 <h1>Tools</h1>
 <ul>
-  <li><SkillEntry skill="Git / GitHub" iconSrc="/icons/skills/git.svg?v=2" iconAlt="Git" /></li>
-  <li><SkillEntry skill="VS Code" iconSrc="/icons/skills/vsc.svg?v=2" iconAlt="VS Code" /></li>
+  <li><SkillEntry skill="Git / GitHub" iconSrc="/htmaa2/desktop/icons/skills/git.svg?v=2" iconAlt="Git" /></li>
+  <li><SkillEntry skill="VS Code" iconSrc="/htmaa2/desktop/icons/skills/vsc.svg?v=2" iconAlt="VS Code" /></li>
 </ul>
 
 
